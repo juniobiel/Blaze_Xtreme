8ea41de20ee8ec313c5ddfb0c36d286e6ea3bbb0
Gabriel Júnio <gabriel.junio@rocketmail.com>
2019-10-16 17:47:58 -0300

    Animação e scripts
    Adição

--- Blaze Xtreme/Assets/Scripts/JogadorUm.cs
+++ Blaze Xtreme/Assets/Scripts/JogadorUm.cs
@@ -10,7 +10,7 @@ public class JogadorUm : MonoBehaviour
     private Animator animacaoJogador;
     public Rigidbody2D jogadorUm;
     //Propriedades do personagem
-    private int vidaJogador, energiaJogador;
+    private int barraDeVida, vidaJogador, barraEnergia;
 
     //contador de tempo
     float contTimerEnergia = 0f;
@@ -18,25 +18,32 @@ public class JogadorUm : MonoBehaviour
     
     void Start()
     {
-        vidaJogador = 100;
-        energiaJogador = 0;
         animacaoJogador = jogadorGrafico.GetComponent<Animator>();
         jogadorUm = this.GetComponent<Rigidbody2D>();
 
         //Define o jogador Parado
         animacaoJogador.SetBool("esta-Parado", true);
+
+        //Define atributos iniciais do personagem
+        vidaJogador = 3;
+        barraDeVida = 100;
+        barraEnergia = 0;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        
         //configurar barra de energia
         //barra de energia deve ser um elemento separado da barra de vida
         contTimerEnergia += Time.deltaTime;
         if(contTimerEnergia >= 5.0f)
         {
-            energiaJogador++;
-            if (energiaJogador > 3) energiaJogador = 3;
+            barraEnergia++;
+            if (barraEnergia > 3) barraEnergia = 3;
             contTimerEnergia = 0;
         }
 
@@ -48,72 +55,96 @@ public class JogadorUm : MonoBehaviour
         //Esta parado
         if (x == 0f && y == 0f)
         {
-            animacaoJogador.SetBool("esta-Parado", true);
-            animacaoJogador.SetBool("andar-Baixo", false);
-            animacaoJogador.SetBool("andar-Cima", false);
-            animacaoJogador.SetBool("andar-Esquerda", false);
-            animacaoJogador.SetBool("andar-Direita", false);
-
-            jogadorUm.velocity = new Vector2(0, 0);
+            estaParado();
         }
         //Andar pra Baixo
-        else if (y < 0f)
+        if (y < 0f)
         {
-            if (x == 0)
-            {
-                animacaoJogador.SetBool("esta-Parado", false);
-                animacaoJogador.SetBool("andar-Baixo", true);
-                animacaoJogador.SetBool("andar-Cima", false);
-                animacaoJogador.SetBool("andar-Esquerda", false);
-                animacaoJogador.SetBool("andar-Direita", false);
-
-                jogadorUm.velocity = new Vector2(x, y) * moduloVelocidade;
-            }
+            andarPraBaixo();
         }
         //Andar pra Cima
-        else if (y > 0f)
+        if (y > 0f)
         {
-            if (x == 0)
-            {
-                animacaoJogador.SetBool("esta-Parado", false);
-                animacaoJogador.SetBool("andar-Cima", true);
-                animacaoJogador.SetBool("andar-Baixo", false);
-                animacaoJogador.SetBool("andar-Esquerda", false);
-                animacaoJogador.SetBool("andar-Direita", false);
-
-                jogadorUm.velocity = new Vector2(x, y) * moduloVelocidade;
-            }
+            andarPraCima();
         }
         //Andar pra Esquerda
         if(x < 0f)
         {
-            if (y == 0)
-            {
-                animacaoJogador.SetBool("esta-Parado", false);
-                animacaoJogador.SetBool("andar-Esquerda", true);
-                animacaoJogador.SetBool("andar-Baixo", false);
-                animacaoJogador.SetBool("andar-Cima", false);
-                animacaoJogador.SetBool("andar-Direita", false);
-
-                jogadorUm.velocity = new Vector2(x, y) * moduloVelocidade;
-                jogadorGrafico.GetComponent<SpriteRenderer>().flipX = true;
-            }
+            andarPraEsquerda();   
         }
         //Andar pra Direita
         if(x > 0f)
         {
-            if(y == 0)
+            andarPraDireita();
+        }
+    }
+
+    void estaParado()
+    {
+        animacaoJogador.SetBool("esta-Parado", true);
+        animacaoJogador.SetBool("andar-Baixo", false);
+        animacaoJogador.SetBool("andar-Cima", false);
+        animacaoJogador.SetBool("andar-Esquerda", false);
+        animacaoJogador.SetBool("andar-Direita", false);
+
+        jogadorUm.velocity = new Vector2(0, 0);
+    }
+
+    void andarPraBaixo()
+    {
+        if(x == 0)
             {
-                animacaoJogador.SetBool("esta-Parado", false);
-                animacaoJogador.SetBool("andar-Direita", true);
-                animacaoJogador.SetBool("andar-Baixo", false);
-                animacaoJogador.SetBool("andar-Cima", false);
-                animacaoJogador.SetBool("andar-Esquerda", false);
+            animacaoJogador.SetBool("esta-Parado", false);
+            animacaoJogador.SetBool("andar-Baixo", true);
+            animacaoJogador.SetBool("andar-Cima", false);
+            animacaoJogador.SetBool("andar-Esquerda", false);
+            animacaoJogador.SetBool("andar-Direita", false);
 
-                jogadorUm.velocity = new Vector2(x, y) * moduloVelocidade;
-                jogadorGrafico.GetComponent<SpriteRenderer>().flipX = false;
+            jogadorUm.velocity = new Vector2(x, y) * moduloVelocidade;
+        }
+    }
 
-            }
+    void andarPraCima()
+    {
+        if (x == 0)
+        {
+            animacaoJogador.SetBool("esta-Parado", false);
+            animacaoJogador.SetBool("andar-Cima", true);
+            animacaoJogador.SetBool("andar-Baixo", false);
+            animacaoJogador.SetBool("andar-Esquerda", false);
+            animacaoJogador.SetBool("andar-Direita", false);
+
+            jogadorUm.velocity = new Vector2(x, y) * moduloVelocidade;
+        }
+    }
+
+    void andarPraEsquerda()
+    {
+        if (y == 0)
+        {
+            animacaoJogador.SetBool("esta-Parado", false);
+            animacaoJogador.SetBool("andar-Esquerda", true);
+            animacaoJogador.SetBool("andar-Baixo", false);
+            animacaoJogador.SetBool("andar-Cima", false);
+            animacaoJogador.SetBool("andar-Direita", false);
+
+            jogadorUm.velocity = new Vector2(x, y) * moduloVelocidade;
+            jogadorGrafico.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    void andarPraDireita()
+    {
+        if (y == 0)
+        {
+            animacaoJogador.SetBool("esta-Parado", false);
+            animacaoJogador.SetBool("andar-Direita", true);
+            animacaoJogador.SetBool("andar-Baixo", false);
+            animacaoJogador.SetBool("andar-Cima", false);
+            animacaoJogador.SetBool("andar-Esquerda", false);
+
+            jogadorUm.velocity = new Vector2(x, y) * moduloVelocidade;
+            jogadorGrafico.GetComponent<SpriteRenderer>().flipX = false;
         }
     }
 }
